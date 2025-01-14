d38dba974340788b933cd011b258c3862e72f343
Harsh Pandey <pandeygautam039@gmail.com>
2025-01-14 08:48:02 +0530

    first commit

---
+++ src/Config/Api.js
@@ -0,0 +1,34 @@
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai";
+
+const apiKey = 'AIzaSyBBFHeOkcnwT6Su93BrJVMjbLSuzxYv8QQ';
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-2.0-flash-exp",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 550,
+  responseMimeType: "text/plain",
+};
+
+async function run(prompt) {
+  const chatSession = model.startChat({
+    generationConfig,
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage(prompt);
+  const response = result.response;
+  return response.text();
+}
+
+export default run;
